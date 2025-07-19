9a8177f2c2bdc97e4557647545e241f146e9f55f
Zhang137-on <103903zyh@gamil.com>
2025-07-19 17:15:54 +0800

    提交人：Zhang 描述： 项目初始化

---
+++ src/main/java/com/zhang/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.zhang.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhang.project.model.entity.User;
+
+/**
+ * @Entity com.yupi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
